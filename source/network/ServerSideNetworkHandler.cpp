30fdacdc5341cbbc4e8749c963912ca5c50aa56f
Malte0621 <50240920+Malte0621@users.noreply.github.com>
2024-09-12 15:48:49 +0200

    Update commands (give/clear)

--- source/network/ServerSideNetworkHandler.cpp
+++ source/network/ServerSideNetworkHandler.cpp
@@ -405,38 +405,67 @@ void ServerSideNetworkHandler::setupCommands()
 	m_commands["time"]  = &ServerSideNetworkHandler::commandTime;
 	m_commands["seed"]  = &ServerSideNetworkHandler::commandSeed;
 	m_commands["tp"]    = &ServerSideNetworkHandler::commandTP;
-	m_commands["give"] = &ServerSideNetworkHandler::commandGIVE;
+	m_commands["give"] = &ServerSideNetworkHandler::commandGive;
+	m_commands["clear"] = &ServerSideNetworkHandler::commandClearInventory;
 }
 
 #include <common/Utils.hpp>
 
-void ServerSideNetworkHandler::commandGIVE(OnlinePlayer* player, const std::vector<std::string>& params)
+void ServerSideNetworkHandler::commandGive(OnlinePlayer* player, const std::vector<std::string>& parms)
 {
-	static bool PrintWarning = false;
-
-	if (PrintWarning == false)
+	if (!m_pLevel)
+		return;
+	if (player->m_pPlayer != this->m_pMinecraft->m_pLocalPlayer)
 	{
-		PrintWarning = true; sendMessage(player, "safe ID's are to 24 index further can be crash because of missing blocks!");
+		sendMessage(player, "Sorry, only the host can use this command at the moment");
+		return;
 	}
-	if (player && m_pLevel && !params.empty())
+	if (parms.size() != 2)
 	{
-		int id = atoi(params[0].c_str());
-		int amount = 1;
-		if (params.size() > 1)
-		{
-			amount = atoi(params[1].c_str());
-		}
+		sendMessage(player, "Usage: /give [item] [item count]");
+		return;
+	}
+	int id = 0;
+	int amount = 1;
+	if (!sscanf(parms[0].c_str(), "%d", &id) || !sscanf(parms[1].c_str(), "%d", &amount))
+	{
+		sendMessage(player, "Usage: /give [item] [item count]");
+		return;
+	}
+	if (amount < 1)
+	{
+		sendMessage(player, "The amount must be greater or equal to 1!");
+		return;
+	}
+	if (Item::items[id] == NULL || id >= 512 || id < 0)
+	{
+		sendMessage(player, "Item \"" + parms[0] + "\" is not implemented!");
+		return;
+	}
 
-		if (eTileID::ITEM_ROCKET < id || eTileID::TILE_AIR > id)
-		{
-			sendMessage(player, "Invalid ID");
-		}
-		ItemInstance itm(id, amount, 0);
-		player->m_pPlayer->m_pInventory->addItem(&itm);
+	Inventory* pInventory = player->m_pPlayer->m_pInventory;
+
+	pInventory->addTestItem(id, amount);
+
+	sendMessage(player, "You have been given " + parms[0] + "x" + parms[1] + ".");
+	return;
+}
+
+void ServerSideNetworkHandler::commandClearInventory(OnlinePlayer* player, const std::vector<std::string>& parms)
+{
+	if (!m_pLevel)
+		return;
+	if (player->m_pPlayer != this->m_pMinecraft->m_pLocalPlayer)
+	{
+		sendMessage(player, "Sorry, only the host can use this command at the moment");
+		return;
 	}
-	else {
-		sendMessage(player, "Requires atleast 1 argument");
-	}
+	Inventory* pInventory = player->m_pPlayer->m_pInventory;
+
+	pInventory->clear();
+
+	sendMessage(player, "Your inventory has been cleared.");
+	return;
 }
 
 void ServerSideNetworkHandler::commandHelp(OnlinePlayer* player, const std::vector<std::string>& parms)
